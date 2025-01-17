6a116b1af4a2ab2d0cae88d0426862d980403889
gajinkee <gajinkee@gmail.com>
2025-01-17 14:53:29 +0800

    Added Task.java class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class Task {
+    private boolean done;
+    private final String name;
+
+    Task(String name) {
+        this.done = false;
+        this.name = name;
+    }
+
+    Task(String name, boolean done) {
+        this.done = done;
+        this.name = name;
+    }
+
+    public Task markTask() {
+        this.done = true;
+        return this;
+    }
+
+    public Task unMarkTask() {
+        this.done = false;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + (this.done? "X" : " ")  + "] " +this.name;
+    }
+
 }
